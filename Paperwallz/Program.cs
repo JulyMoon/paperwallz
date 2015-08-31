2568168f4a1bb8ddbb8f38c82851e2a44b3a7a14
JulyMoon <JulyMoon@users.noreply.github.com>
2015-08-31 16:14:54 +0300

    only one program instance

--- Paperwallz/Program.cs
+++ Paperwallz/Program.cs
@@ -1,16 +1,35 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Paperwallz
 {
     static class Program
     {
+        static readonly Mutex mutex = new Mutex(false, "Never forget. Don't let your dreams be dreams. - foxneZz 8/31/2015");
+        // http://stackoverflow.com/a/6486819/1412924
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainWindow());
+
+            if (!mutex.WaitOne(TimeSpan.FromSeconds(2), false))
+            {
+                MessageBox.Show("Another instance is already running.", "Paperwallz",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            try
+            {
+                Application.Run(new MainWindow());
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
         }
     }
 }
