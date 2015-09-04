96a9a8657670d386dbaa47fb35d6bf543cdd7e4c
JulyMoon <JulyMoon@users.noreply.github.com>
2015-09-04 18:07:37 +0300

    about window now hides after clicking on email

--- Paperwallz/AboutWindow.cs
+++ Paperwallz/AboutWindow.cs
@@ -10,8 +10,22 @@ namespace Paperwallz
         {
             InitializeComponent();
             nameLabel.Text = Application.ProductName;
-            versionLabel.Text = Application.ProductVersion;
+            versionLabel.Text = Format(Application.ProductVersion);
             authorLabel.Text = Application.CompanyName;
+        }
+
+        private static string Format(string version)
+        {
+            var digits = version.Split('.');
+
+            version = digits[0] + "." + digits[1];
+
+            if (digits[3] != "0")
+                version += "." + digits[2] + "." + digits[3];
+            else if (digits[2] != "0")
+                version += "." + digits[2];
+
+            return version;
         }
 
         private void okButton_Click(object sender, EventArgs e)
@@ -22,6 +36,7 @@ namespace Paperwallz
         private void emailLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             Process.Start("mailto://foxnezz@gmail.com");
+            Hide();
         }
     }
 }
